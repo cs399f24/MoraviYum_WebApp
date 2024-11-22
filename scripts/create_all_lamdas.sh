0be2655b8cfdc5bba691f6ca647de2398b4a033d
Michael Romero <romerom@moravian.edu>
2024-11-22 01:42:05 -0500

    Added new script to create_all_lamdas script

--- scripts/create_all_lamdas.sh
+++ scripts/create_all_lamdas.sh
@@ -1,6 +1,8 @@
 python lamdba_fetch_vendor_foods.py
 python lambda_get_reviews.py
 python lambda_submit_review.py
+python lambda_reviews.py
 ./create_lambda_fetch_vendor_foods.sh
 ./create_lambda_get_reviews.sh
 ./create_lambda_submit_review.sh
+./create_lambda_review.sh
