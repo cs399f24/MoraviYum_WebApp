a81a558b88e495d2c9f16796dea31805857b5e12
Michael Romero <romerom@moravian.edu>
2024-11-22 00:05:42 -0500

    Made a script to create all lamda functions

---
+++ scripts/create_all_lamdas.sh
@@ -0,0 +1,6 @@
+python lamda_fetch_vendor_foods.py
+python lamda_get_reviews.py
+python lamda_submit_review.py
+./create_lambda_fetch_vendor_foods.sh
+./create_lambda_get_reviews.sh
+./create_lambda_submit_review.sh
