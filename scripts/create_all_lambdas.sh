29ab677b1f0bd17646e0906b595da057ab517d48
Rafael Garcia Jr. <68786066+RGJ-713@users.noreply.github.com>
2024-12-03 21:25:38 -0500

    Update create_all_lambdas.sh

--- scripts/create_all_lambdas.sh
+++ scripts/create_all_lambdas.sh
@@ -4,9 +4,11 @@ python lambda_submit_review.py
 python lambda_review.py
 python lambda_get_user_handle.py
 python lambda_store_user_handle.py
+python lambda_menu.py
 ./create_lambda_fetch_vendor_foods.sh
 ./create_lambda_get_reviews.sh
 ./create_lambda_submit_review.sh
 ./create_lambda_review.sh
 ./create_lambda_get_user_handle.sh
 ./create_lambda_store_user_handle.sh
+./create_lambda_menu.sh
