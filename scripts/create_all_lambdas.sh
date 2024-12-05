bff5406006c43291c0cc266982ccbab46d2dcd95
EC2 Default User <ec2-user@ip-172-31-76-5.ec2.internal>
2024-12-05 22:49:41 +0000

    Completed API and create_all_lambdas scripts

--- scripts/create_all_lambdas.sh
+++ scripts/create_all_lambdas.sh
@@ -1,14 +1,8 @@
-python lambda_fetch_vendor_foods.py
-python lambda_get_reviews.py
-python lambda_submit_review.py
-python lambda_review.py
-python lambda_get_user_handle.py
-python lambda_store_user_handle.py
-python lambda_menu.py
+python fetch_vendor_foods.py
+python menu.py
+python get_reviews.py
+python submit_review.py
 ./create_lambda_fetch_vendor_foods.sh
+./create_lambda_menu.sh
 ./create_lambda_get_reviews.sh
 ./create_lambda_submit_review.sh
-./create_lambda_review.sh
-./create_lambda_get_user_handle.sh
-./create_lambda_store_user_handle.sh
-./create_lambda_menu.sh
